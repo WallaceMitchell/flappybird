1b95b1fb6ee91f663267ef3412cf298ba11ceec9
sofiabtt <110426682+sofiabtt@users.noreply.github.com>
2024-11-01 21:50:40 -0300

    Add files via upload

--- tubo_movimiento.asm
+++ tubo_movimiento.asm
@@ -1,105 +1,136 @@
 .ORIG x3000
 
-LD R0, inicio
-LD R1, negro
-LD R2, verde
-LD R4, columna
-LD R5, enter
-LD R6, inicio2
-LD R7, columna2
+LD R0, inicio       ; Primer pixel de la pantalla
+LD R1, celeste      ; Celeste
+LD R2, pixeles      ; Cant de pixeles
+LD R3, ancho        ; Ancho pantalla 
+LD R4, inicio_columna ; Primer pixel de la columna
 
-LOOP
 
-TUBO1
-STR R2, R0, #0
-ADD R0, R0, R5
-ADD R4, R4, #-1
-BRp TUBO1
 
-TUBO2
-STR R2, R6, #0
-ADD R6, R6, R5
-ADD R7, R7, #-1
-BRp TUBO2
+; Fondo celeste
+FONDO
+STR R1, R0, #0      ; Pintar pixel de celeste
+ADD R0, R0, #1      ; Mueve al siguiente pixel
+ADD R2, R2, #-1     ; REduce el cont de pixeles
+BRp FONDO
 
-JSR DELAY
+LD R2,ancho
+LD R3,dieciocho
+ADD R2,R2,R3     
+ST R2,GUARDAR_R2 
+BRnzp MAIN
+REINICIAR_CONT
+LD R2,ancho
+LD R3,dieciocho
+ADD R2,R2,R3
+ST R2,GUARDAR_R2
+ADD R4,R4,R2
+SALTAR
+MAIN
+JSR TUBO
 
-LD R4, columna
-LD R7, columna2
+ATRASAR
+LD R1,inicio
+ADD R1,R1,#-1
+BRp ATRASAR
 
-NOT R5, R5
-ADD R5, R5, #1
+JSR BORRAR_TUBO
+ADD R4,R4,#-1
+LD R2,GUARDAR_R2
+ADD R2,R2,#-1
+ST R2,GUARDAR_R2
+BRp MAIN
+BRnzp REINICIAR_CONT
 
-RESTART1
-ADD R0, R0, R5
-ADD R4, R4, #-1
-BRp RESTART1
-
-RESTART2
-ADD R6, R6, R5
-ADD R7, R7, #-1
-BRp RESTART2
-
-LD R4, columna
-LD R7, columna2
-
-NOT R5, R5
-ADD R5, R5, #1
-
-TUBO3
-STR R1, R0, #0
-ADD R0, R0, R5
-ADD R4, R4, #-1
-BRp TUBO3
-
-TUBO4
-STR R1, R6, #0
-ADD R6, R6, R5
-ADD R7, R7, #-1
-BRp TUBO4
-
-LD R4, columna
-LD R7, columna2
-
-NOT R5, R5
-ADD R5, R5, #1
-
-RESTART3
-ADD R0, R0, R5
-ADD R4, R4, #-1
-BRp RESTART3
-
-RESTART4
-ADD R6, R6, R5
-ADD R7, R7, #-1
-BRp RESTART4
-
-NOT R5, R5
-ADD R5, R5, #1
-
-LD R4, columna
-LD R7, columna2
-
-ADD R0, R0, #-1
-ADD R6, R6, #-1
-
-BRnzp LOOP
-
-DELAY
-LD R3, delay_val
-DELAY_LOOP
-ADD R3, R3, #-1
-BRp DELAY_LOOP
+BORRAR_TUBO
+LD R1, celeste
+LD R3, ancho        ; Desplazamiento para la siguiente fila (ancho de la pantalla)
+LD R6	,dieciocho
+ADD R3,R3,R6
+LD R2, ancho
+ADD R2,R2,#14  
+ST R4,GUARDAR_R4
+ADD R4,R4,R6
+LUPP
+STR R1, R4, #1
+ADD R4,R4,R3
+ADD R2,R2,#-1
+BRp LUPP
+LD R4,GUARDAR_R4
 RET
 
-HALT
+TUBO
+LD R1, verde        ; Cargamos el color verde
+LD R5, negro        ; Negro
+ST R4,GUARDAR_R4
+LD R2, cuarentaydos ; Altura de la columna verde
+LD R3, ancho        ; Desplazamiento para la siguiente fila (ancho de la pantalla)
 
-inicio .FILL xC07F
-inicio2 .FILL xE57F
+LOOP1
+STR R5, R4, #-1                 ; Pintar columna verde
+LD R6, dieciocho              
+PINTO
+STR R1, R4, #0
+ADD R4, R4, #1
+ADD R6, R6, #-1
+BRp PINTO
+STR R5, R4, #0
+
+ADD R4, R4, R3      ; Mover a la siguiente fila para la columna
+ADD R2, R2, #-1     ; Reducir contador de altura de la columna
+BRp LOOP1           ; Repetir mientras queden filas en la columna
+
+STR R5, R4, #-1
+LD R6, dieciocho              
+PINTO11
+STR R5, R4, #0
+ADD R4, R4, #1
+ADD R6, R6, #-1
+BRp PINTO11
+STR R5, R4, #0
+
+
+LD R2, espacio ; Cargo 40 pixeles de espacio
+ADD R4, R4, R2     
+
+STR R5, R4, #-1
+LD R6, dieciocho              
+PINTO21
+STR R5, R4, #0
+ADD R4, R4, #1
+ADD R6, R6, #-1
+BRp PINTO21
+STR R5, R4, #0
+
+ADD R4, R4, R3    
+
+LD R2, cuarentaydos ; Recargo altura columna
+LOOP2
+STR R5, R4, #-1                 ; Pintar columna verde
+LD R6, dieciocho              
+PINTO2
+STR R1, R4, #0
+ADD R4, R4, #1
+ADD R6, R6, #-1
+BRp PINTO2
+STR R5, R4, #0
+
+ADD R4, R4, R3      ; Mover a la siguiente fila para la columna
+ADD R2, R2, #-1     ; Reducir contador de altura de la columna
+BRp LOOP2           ; 
+LD R4,GUARDAR_R4
+RET
+inicio .FILL xC000  ; Primer pixel
+celeste .FILL x07FF ; Celeste
+verde .FILL x03A0   ; Verde 
+cuarentaydos .FILL #42  ; Altura columna
+pixeles .FILL #15872    ; Cant pixeles totales
+ancho .FILL #110        ; Ancho pantalla
+abajo .FILL x80         ; Baja de fila
+inicio_columna .FILL xC06C ; Primer pixel de la columna
+espacio .FILL #4974 ; Espacio entre columnas
 negro .FILL x0000
-verde .FILL x03E0
-delay_val .FILL #9000
-enter .FILL #128
-columna .FILL #50
-columna2 .FILL #50
-.END
+dieciocho .FILL #18
+GUARDAR_R4  	.BLKW 1
+GUARDAR_R2  	.BLKW 1
